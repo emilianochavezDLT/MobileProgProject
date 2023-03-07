4d09a8d4238af0edf7ced508fe415e04e90045f6
DotCh2 <emilianochavezdelatorre@gmail.com>
2023-03-06 20:02:22 -0700

    progress on the side bar

--- mainPageJS.js
+++ mainPageJS.js
@@ -16,7 +16,25 @@ function closeVisibilty(id) {
 //The open function will take an any id as a parameter and unhide the contents of that id
 function openVisibilty(id) {
 
-    document.getElementById(id).style.display = "block"; //Unhides the contents of the id
+    if(document.getElementById(id).style.display === "none"){
+    
+        document.getElementById(id).style.display = "block"; //Unhides the contents of the id
+
+    }
+
+    if(document.getElementById(id) === "sideBar"){
+
+        toggleSideBar(id);
+    
+    }
+
+
+
 }
 
+//Toggling the width of the sideBar
+function toggleSideBar(id) {
 
+    document.getElementById(id).style.width = "250px"; //Unhides the contents of the id
+
+}
