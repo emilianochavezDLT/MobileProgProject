c823f704cecebed545c4e86f703759d717082e16
jayasaied <98234031+jayasaied@users.noreply.github.com>
2023-03-11 15:22:08 -0700

    Migrated Create Playlist, Linked Pages

---
+++ createPage.js
@@ -0,0 +1,40 @@
+// Main Page Javascript
+
+
+//The closeForm function will close the form and hide all of its contents 
+function closeVisibilty(id) {
+
+    if(document.getElementById(id).style.display === "block"){
+
+        document.getElementById(id).style.display = "none"; //Unhides the contents of the id
+        
+    }
+    
+
+}
+
+//The open function will take an any id as a parameter and unhide the contents of that id
+function openVisibilty(id) {
+
+    if(document.getElementById(id).style.display === "none"){
+    
+        document.getElementById(id).style.display = "block"; //Unhides the contents of the id
+
+    }
+
+    if(document.getElementById(id) === "sideBar"){
+
+        toggleSideBar(id);
+    
+    }
+
+
+
+}
+
+//Toggling the width of the sideBar
+function toggleSideBar(id) {
+
+    document.getElementById(id).style.width = "250px"; //Unhides the contents of the id
+
+}
