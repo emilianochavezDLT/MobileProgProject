39f7b9751820166ad04fddfbcf2768ac8c2ffd11
broganb <70031301+broganb@users.noreply.github.com>
2023-04-22 21:34:13 -0600

    Cleaning Up Code

--- app.js
+++ app.js
@@ -2,26 +2,24 @@
 //Testing the server with api for our workout app
 
 //Importing the express module
-const express = require('express');
+const express = require("express");
 //Import uuid
 //const uuid = require('uuid'); //We are going to keep it basic for now and just use our id as a number
 //Importing the body-parser module
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
 
 //Creating an instance of express
 const app = express();
 
 //Setting up the static folder to serve the front end web pages
 
-//This is going to be the folder that is going to hold the front end web pages, 
+//This is going to be the folder that is going to hold the front end web pages,
 //we can just ignore this for now since. It was just to test out the server.
 //app.use(express.static('public'));
 
-
 //Middleware
 //This is going to be used to parse the data that the user sends to the server
-//We are going to use the json method to parse the data, 
+//We are going to use the json method to parse the data,
 //meaning that we are going to parse the data as json, and not as url encoded data
 app.use(bodyParser.json());
 
@@ -48,10 +46,10 @@ const workouts = [
         description: 'Bench Press, Dips, Push Ups',
     },
 
-*/ 
+*/
 
 //Creating an array of objects
-const workouts = []
+const workouts = [];
 
 /* The functions below have two arguments/parameter.
  
@@ -75,29 +73,29 @@ service.
 
 */
 
-
-
 //Creating a route for the api
-app.get('/workouts', (req, res) => {
-    res.json(workouts);
+app.get("/workouts", (req, res) => {
+  res.json(workouts);
 });
-
 
 /*
 This is a get request for a single workout. We are going to use the some method to check
 if the id that the user is looking for is in the array. If the id is in the array then we
 are going to return the workout. If the id is not in the array then we are going to return
 a 400 status code and a message that says that the workout was not found.
-*/ 
-app.get('/workouts/:id', (req, res) => {
-    const found = workouts.some(workout => workout.id === parseInt(req.params.id));
-    if (found) {
-        res.json(workouts.filter(workout => workout.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({ msg: `No workout with the id of ${req.params.id}` });
-    }
+*/
+app.get("/workouts/:id", (req, res) => {
+  const found = workouts.some(
+    (workout) => workout.id === parseInt(req.params.id)
+  );
+  if (found) {
+    res.json(
+      workouts.filter((workout) => workout.id === parseInt(req.params.id))
+    );
+  } else {
+    res.status(400).json({ msg: `No workout with the id of ${req.params.id}` });
+  }
 });
-
 
 /*
 
@@ -107,80 +105,80 @@ We are going to use the push method to add the new workout to the array. We are 
 use the json method to send the new workout back to the user.
 
 */
-app.post('/workouts', (req, res) => {
-    const newWorkout = req.body;
-    console.log(req.body)
-    console.log('Received: ',newWorkout);
-    //Creating a variable to hold the id of the new workout
-    const id = workouts.length + 1;
-    //Adding the id to the new workout
-    newWorkout.id = id;
-    //Adding the new workout to the array
-    workouts.push(newWorkout);
-    //Sending the new workout back to the user
-    res.json(newWorkout);
+app.post("/workouts", (req, res) => {
+  const newWorkout = req.body;
+  console.log(req.body);
+  console.log("Received: ", newWorkout);
+  //Creating a variable to hold the id of the new workout
+  const id = workouts.length + 1;
+  //Adding the id to the new workout
+  newWorkout.id = id;
+  //Adding the new workout to the array
+  workouts.push(newWorkout);
+  //Sending the new workout back to the user
+  res.json(newWorkout);
 });
 
 /**
  * Making a post request for an already existing workout
  * If the workout already exists then we just want to update the workout
  * We are not going to change the name of the workout, but we are going to change the
- * description of the workout and just add to the description 
+ * description of the workout and just add to the description
  */
 
-app.post('/workouts/:id', (req, res) => {
-    //Checking if the workout exists by using the some method
-    const found = workouts.some(workout => workout.id === parseInt(req.params.id));
-    //If the workout is found then we are going to update the workout
-    if (found) {
-
-        //Creating a variable to hold the updated workout
-        const updWorkout = req.body;
-        //Looping through the array to find the workout that the user is looking for
-        workouts.forEach(workout => {
-            //Checking if the id matches the id that the user is looking for
-            if (workout.id === parseInt(req.params.id)) {
-                //Updating the workout
-                workout.description = updWorkout.description ? updWorkout.description : workout.description;
-                //Sending the updated workout back to the user
-                res.json({ msg: 'Workout updated', workout });
-            }
-        });
-    } else {
-        res.status(400).json({ msg: `No workout with the id of ${req.params.id}` });
-    }
+app.post("/workouts/:id", (req, res) => {
+  //Checking if the workout exists by using the some method
+  const found = workouts.some(
+    (workout) => workout.id === parseInt(req.params.id)
+  );
+  //If the workout is found then we are going to update the workout
+  if (found) {
+    //Creating a variable to hold the updated workout
+    const updWorkout = req.body;
+    //Looping through the array to find the workout that the user is looking for
+    workouts.forEach((workout) => {
+      //Checking if the id matches the id that the user is looking for
+      if (workout.id === parseInt(req.params.id)) {
+        //Updating the workout
+        workout.description = updWorkout.description
+          ? updWorkout.description
+          : workout.description;
+        //Sending the updated workout back to the user
+        res.json({ msg: "Workout updated", workout });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No workout with the id of ${req.params.id}` });
+  }
 });
-
-
 
 //Creating a put request. This is going to be used to update a workout in the array.
 //We are going to use the map method to loop through the array and check if the id matches
 //the id that the user is looking for. If the id matches then we are going to update the
 //workout. If the id does not match then we are going to return the workout.
-app.put('/workouts/:id', (req, res) => {
+app.put("/workouts/:id", (req, res) => {
+  //Checking if the workout exists by using the some method
+  /**
+   * The some method is going to loop through the array and check if the id that the user
+   * is looking for is in the array. If the id is in the array then the some method is going
+   * to return true. If the id is not in the array then the some method is going to return
+   * false.
+   */
+  const found = workouts.some(
+    (workout) => workout.id === parseInt(req.params.id)
+  );
 
-    //Checking if the workout exists by using the some method
-    /**
-     * The some method is going to loop through the array and check if the id that the user
-     * is looking for is in the array. If the id is in the array then the some method is going
-     * to return true. If the id is not in the array then the some method is going to return
-     * false.
-     */
-    const found = workouts.some(workout => workout.id === parseInt(req.params.id)); 
+  //If the workout is found then we are going to update the workout
+  if (found) {
+    //Creating a variable to hold the updated workout
+    const updWorkout = req.body;
 
-    //If the workout is found then we are going to update the workout
-    if (found) {
-        //Creating a variable to hold the updated workout
-        const updWorkout = req.body;
-
-        //Looping through the array and updating the workout
-        workouts.forEach(workout => {
-
-            //Checking if the id matches the id that the user is looking for
-            if (workout.id === parseInt(req.params.id)) {
-
-                //Updating the workout
-                /**
+    //Looping through the array and updating the workout
+    workouts.forEach((workout) => {
+      //Checking if the id matches the id that the user is looking for
+      if (workout.id === parseInt(req.params.id)) {
+        //Updating the workout
+        /**
                  
                 *This reads as follows:
                 * Object.keys(updWorkout) is going to return an array of the keys in the
@@ -189,22 +187,20 @@ app.put('/workouts/:id', (req, res) => {
                 *   
                 *  
                 */
-                Object.keys(updWorkout).forEach(key => {
-                    workout[key] = updWorkout[key];
-                });
-
-                //Sending the updated workout back to the user
-                res.json({ msg: 'Workout updated', workout });
-            }
+        Object.keys(updWorkout).forEach((key) => {
+          workout[key] = updWorkout[key];
         });
-    } 
-    
-    //If the workout is not found then we are going to return a 400 status code and a message
-    else {
-        res.status(400).json({ msg: `No workout with the id of ${req.params.id}` });
-    }
 
+        //Sending the updated workout back to the user
+        res.json({ msg: "Workout updated", workout });
+      }
+    });
+  }
 
+  //If the workout is not found then we are going to return a 400 status code and a message
+  else {
+    res.status(400).json({ msg: `No workout with the id of ${req.params.id}` });
+  }
 });
 
 //Creating a delete request. This is going to be used to delete a workout user from the array.
@@ -212,45 +208,39 @@ app.put('/workouts/:id', (req, res) => {
 //the id that the user is looking for. If the id matches then we are going to delete the
 //workout. If the id does not match then we are going to return the workout.
 
-app.delete('/workouts/:id', (req, res) => {
+app.delete("/workouts/:id", (req, res) => {
+  //Checking if the workout exists by using the some method
+  /**
+   * The some method is going to loop through the array and check if the id that the user
+   * is looking for is in the array. If the id is in the array then the some method is going
+   * to return true. If the id is not in the array then the some method is going to return
+   * false.
+   *
+   *
+   */
+  let found = workouts.some(
+    (workout) => workout.id === parseInt(req.params.id)
+  );
 
-    //Checking if the workout exists by using the some method
-    /**
-     * The some method is going to loop through the array and check if the id that the user
-     * is looking for is in the array. If the id is in the array then the some method is going
-     * to return true. If the id is not in the array then the some method is going to return
-     * false.
-     * 
-     * 
-     */
-    let found = workouts.some(workout => workout.id === parseInt(req.params.id));
+  //If the workout is found then we are going to delete the workout
+  if (found) {
+    //Deleting the workout by using the splice method
+    //The splice method is going to take two arguments. The first argument is going to be
+    //the index of the workout that we want to delete. The second argument is going to be
+    //the number of workouts that we want to delete. In this case we are only going to delete
+    //one workout.
+    workouts.splice(
+      workouts.findIndex((workout) => workout.id === parseInt(req.params.id)),
+      1
+    );
 
-    //If the workout is found then we are going to delete the workout
-    if (found) {   
-        
-        //Deleting the workout by using the splice method
-        //The splice method is going to take two arguments. The first argument is going to be
-        //the index of the workout that we want to delete. The second argument is going to be
-        //the number of workouts that we want to delete. In this case we are only going to delete
-        //one workout.
-        workouts.splice(workouts.findIndex(workout => workout.id === parseInt(req.params.id)), 1);
-
-        //Sending the updated array back to the user
-        res.json({ msg: 'User deleted', workouts });
-
-    } else {
-        res.status(400).json({ msg: `No workout with the id of ${req.params.id}` });
-    }
+    //Sending the updated array back to the user
+    res.json({ msg: "User deleted", workouts });
+  } else {
+    res.status(400).json({ msg: `No workout with the id of ${req.params.id}` });
+  }
 });
 
-
-
 //We have to change the following below once we start hosting
-app.listen(3000, () => console.log('Server started on port 3000'));
-console.log('http://localhost:3000');
-
-
-
-
-
-
+app.listen(3000, () => console.log("Server started on port 3000"));
+console.log("http://localhost:3000");
--- mainPageJS.js
+++ mainPageJS.js
@@ -1,140 +1,110 @@
 // Main Page Javascript
 
-
-//The closeForm function will close the form and hide all of its contents 
+//The closeForm function will close the form and hide all of its contents
 function closeVisibilty(id) {
+  if (document.getElementById(id).style.display === "block") {
+    document.getElementById(id).style.display = "none"; //Unhides the contents of the id
 
-    if(document.getElementById(id).style.display === "block"){
-
-        document.getElementById(id).style.display = "none"; //Unhides the contents of the id
-
-        if(id === "sideBar"){
-
-            closeSideBar(id);           
-        
-        }
-        
+    if (id === "sideBar") {
+      closeSideBar(id);
     }
-    
-
+  }
 }
 
 //The open function will take an any id as a parameter and unhide the contents of that id
 function openVisibilty(id) {
+  if (document.getElementById(id).style.display === "none") {
+    document.getElementById(id).style.display = "block"; //Unhides the contents of the id
 
-    if(document.getElementById(id).style.display === "none"){
-    
-        document.getElementById(id).style.display = "block"; //Unhides the contents of the id
-
-        //If side bar is opened, then view the list of workouts
-        if(id === "sideBar"){
-
-            toggleSideBar(id);
-        
-        }
-
+    //If side bar is opened, then view the list of workouts
+    if (id === "sideBar") {
+      toggleSideBar(id);
     }
-
+  }
 }
 
 //Toggling the width of the sideBar
 function toggleSideBar(id) {
-
-    document.getElementById(id).style.width = "250px"; //Unhides the contents of the id
-
+  document.getElementById(id).style.width = "250px"; //Unhides the contents of the id
 }
 
 //Closes the sideBar
 function closeSideBar(id) {
-
-    document.getElementById(id).style.width = "0px"; //Unhides the contents of the id
-
+  document.getElementById(id).style.width = "0px"; //Unhides the contents of the id
 }
-
 
 //creating a list that takes a input text and adds it as a title.
 //then creating a ul and appending the workouts to the list
 function createList(playistTitle, Description, parentList, sideBar) {
+  //Getting the input text from the playlistTitle text box
+  let title = document.getElementById(playistTitle).value;
 
-    //Getting the input text from the playlistTitle text box
-    let title = document.getElementById(playistTitle).value;
+  //Parent element which is the div in this case
+  let parent = document.getElementById(parentList);
+  parent.innerHTML = "<h3>" + title + "</h3>"; //Clearing the parent element
 
-    //Parent element which is the div in this case
-    let parent = document.getElementById(parentList);
-    parent.innerHTML = "<h3>" + title +"</h3>"; //Clearing the parent element
-    
-    let tempSideBar = document.getElementById(sideBar);
-    tempSideBar.style.display = "block"; //Unhides the contents of the id
+  let tempSideBar = document.getElementById(sideBar);
+  tempSideBar.style.display = "block"; //Unhides the contents of the id
 
-    //getting the textbox input and using a delimter to split the string into the workouts array
-    let textboxInput = document.getElementById(Description).value;
-    let workouts = textboxInput.split(","); //Splitting the string into an array
+  //getting the textbox input and using a delimter to split the string into the workouts array
+  let textboxInput = document.getElementById(Description).value;
+  let workouts = textboxInput.split(","); //Splitting the string into an array
 
-     //creating the ul element
-     let playlistUl = document.createElement("ul");
-    //Adding the ul element to the parent element
-    parent.appendChild(playlistUl);
+  //creating the ul element
+  let playlistUl = document.createElement("ul");
+  //Adding the ul element to the parent element
+  parent.appendChild(playlistUl);
 
-    //Adding the li elements to the ul element
-    for (let index = 0; index < workouts.length; index++) {
-        let wI = document.createElement("li");
-        wI.innerHTML = workouts[index];
-        playlistUl.appendChild(wI);
-    }
+  //Adding the li elements to the ul element
+  for (let index = 0; index < workouts.length; index++) {
+    let wI = document.createElement("li");
+    wI.innerHTML = workouts[index];
+    playlistUl.appendChild(wI);
+  }
 
-    tempSideBar.style.display = "none"; //Unhides the contents of the id
-
+  tempSideBar.style.display = "none"; //Unhides the contents of the id
 }
-
 
 /** Add exercise function will add a new input box to the form
  * where it will allow the user to add more exercises to the list
  *
  * It will have a button that will add a new input box
  * and a button that will remove the last input box.
- * 
+ *
  */
 
 let exerciseIncrement = 2;
 //Creating a new input box
 function addExercise(parent) {
-    
-    
-    
-    //Getting the parent element which is the div called exerciseBoxes
-    let exerciseDiv = document.getElementById(parent);
-    let exercise = document.createElement("input");
-    exercise.setAttribute("type", "text");
-    exercise.setAttribute("name", "exercise" + exerciseIncrement);
-    exercise.setAttribute("id", "exercise" + exerciseIncrement);
-    exercise.setAttribute("placeholder", "Exercise " + exerciseIncrement);
-    exercise.setAttribute("class", "exercise");
+  //Getting the parent element which is the div called exerciseBoxes
+  let exerciseDiv = document.getElementById(parent);
+  let exercise = document.createElement("input");
+  exercise.setAttribute("type", "text");
+  exercise.setAttribute("name", "exercise" + exerciseIncrement);
+  exercise.setAttribute("id", "exercise" + exerciseIncrement);
+  exercise.setAttribute("placeholder", "Exercise " + exerciseIncrement);
+  exercise.setAttribute("class", "exercise");
 
-    //We are going to add a button that will remove the last input box
-    let removeExercise = document.createElement("button");
-    removeExercise.setAttribute("type", "button");
-    removeExercise.setAttribute("id", "removeExercise" + exerciseIncrement);
-    removeExercise.setAttribute("class", "cancelBtn");
-    removeExercise.innerHTML = "X";
+  //We are going to add a button that will remove the last input box
+  let removeExercise = document.createElement("button");
+  removeExercise.setAttribute("type", "button");
+  removeExercise.setAttribute("id", "removeExercise" + exerciseIncrement);
+  removeExercise.setAttribute("class", "cancelBtn");
+  removeExercise.innerHTML = "X";
 
-    //Here we are going to add an event listener to the button
-    //This will remove the last input box
-    removeExercise.addEventListener("click", function() {
-        exerciseDiv.removeChild(exercise);
-        exerciseDiv.removeChild(removeExercise);
-        //Decrementing so we can keep track of the number of input boxes
-        exerciseIncrement--;
-    });
+  //Here we are going to add an event listener to the button
+  //This will remove the last input box
+  removeExercise.addEventListener("click", function () {
+    exerciseDiv.removeChild(exercise);
+    exerciseDiv.removeChild(removeExercise);
+    //Decrementing so we can keep track of the number of input boxes
+    exerciseIncrement--;
+  });
 
+  //Adding the input box to the parent element
+  exerciseDiv.appendChild(exercise);
+  exerciseDiv.appendChild(removeExercise);
 
-
-
-    //Adding the input box to the parent element    
-    exerciseDiv.appendChild(exercise);
-    exerciseDiv.appendChild(removeExercise);
-
-    //Incrementing the exerciseIncrement
-    exerciseIncrement++;
-
-
+  //Incrementing the exerciseIncrement
+  exerciseIncrement++;
 }
